4d5c6344db5d1597d523cd5bac9748945c1e0921
weirdASD <167679316+weirdASD@users.noreply.github.com>
2024-04-25 17:24:50 +0300

    image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -10,148 +10,166 @@ Original file is located at
 """
 
 
+import io
 import streamlit as st
 import pandas as pd
-from scipy.spatial.distance import pdist
-#from scipy.cluster.hierarchy import *
-from matplotlib import pyplot as plt
-from matplotlib import rc
 import numpy as np
-from sklearn.cluster import KMeans
+import sklearn.preprocessing
+import sklearn.feature_extraction
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import warnings
+warnings.filterwarnings('ignore')
+import scipy.stats
 
 # Загрузка данных
 def load_data():
     # Используйте полный путь к вашему файлу
-    data = pd.read_excel(r'C:\Users\Сергей\Desktop\project\wb_teapot.xlsx')
+    data = pd.read_csv('/content/star.csv', encoding='utf-8')
+    data.drop_duplicates(inplace=True)
+    data = data.reset_index()
     return data
 
 # Функция для отображения описания полей данных
-def show_data_description(df):
+def show_data_description(data):
     st.subheader("Описание полей данных:")
     st.write("Этот раздел содержит описание каждого поля в исходных данных.")
     st.write("Пожалуйста, ознакомьтесь с ним перед анализом данных.")
-
-    # Описание полей
-    df_description = {
-        "id": "Номер",
-        "Review": "Количество отзывов",
-        "Star": "Оценка (0 - не было оценок)",
-        "Value": "Стоимость с учетом скидки",
-        "brandId": "Код бренда",
-        "brandName": "Бренд",
-        "goodsName": "Наименование товара",
-        "isSoldOut": "Распродажа",
-        "link": "Ссылка",
-        "lowQuantity": "Мало в остатках",
-        "ordersCount": "Исторические продажи, штук, с момента появления на маркетплейсе",
-        "price": "Стоимость без скидок",
-        "qualityRate": "Оценка качества/ удовлетворенность пользователей",
-        "Вес с упаковкой (кг)": "Вес с упаковкой (кг)",
-        "Длина кабеля": "Длина кабеля",
-        "Количество температурных режимов": "Количество температурных режимов",
-        "Материал корпуса": "Материал корпуса",
-        "Модель": "Модель",
-        "Мощность устройства": "Мощность устройства",
-        "Объем чайника": "Объем чайника",
-        "Страна бренда": "Страна бренда",
-        "Страна производитель": "Страна производитель",
-        "Цвет": "Цвет",
-        "sale_june": "Продажи за июнь 2020 года"
-    }
-
-    # Вывод описания полей
-    for column, description in df_description.items():
-        st.write(f"**{column}**: {description}")
+    data.rename(columns={'Temperature (K)': 'Температура_К', 
+                   'Luminosity(L/Lo)': 'Свечение_л_ло', 
+                   'Radius(R/Ro)': 'Радиус_р_ро',
+                   'Absolute magnitude(Mv)': 'Абсолют_магнитизм_мв',
+                   'Star type': 'Тип_звезды',
+                   'Star color': 'Цвет_звезды',
+                   'Spectral Class': 'Спектральный_класс'}, 
+          inplace=True) 
+    st.write(data)
 
 # Функция для анализа и предобработки данных
-def data_analysis(df):
+def data_analysis(data):
     st.subheader("Анализ и предобработка данных:")
     st.write("Этот раздел содержит анализ и предобработку данных перед их использованием в анализе и визуализации.")
 
     # Поиск нулевых значений
     st.write("### Нулевые значения:")
-    st.write(df.isnull().sum())
+    st.write(data.isnull().sum())
 
     # Количество уникальных значений для любого столбца
     st.write("### Количество уникальных значений для каждого столбца:")
-    st.write(df.nunique())
+    st.write(data.nunique())
 
     # Описательная статистика данных
     st.write("### Описательная статистика данных:")
-    st.write(df.describe())
+    st.write(data.describe())
 
-# Функция для подготовки
-def data_preporation(df):
-    st.subheader("Подготовка данных:")
-    st.write("Этот раздел содержит подготовку данных для кластерного анализа.")
+    # Количество всех категорий в виде графиков
+    st.write("### Количество всех категорий в виде графиков:")
+    categorical_columns = data.select_dtypes(include=['object']).columns.tolist()
+    for column in categorical_columns:
+        plt.figure(figsize=(10, 6))
+        sns.countplot(x=column, data=data)
+        plt.title(f"Распределение значений по категориям в столбце '{column}'")
+        plt.xticks(rotation=45)
+        plt.xlabel(column)
+        plt.ylabel("Количество")
+        st.pyplot(plt)
 
-    col=['Review', 'Star', 'ordersCount']
-    st.write('Колонки, которые мы будем использовать:', col)
-    pd.options.mode.chained_assignment = None 
-    df[col].fillna(0, inplace=True)
-    
-    st.write("Таблица корреляции:")
-    from pandas.plotting import scatter_matrix
-    scatter_matrix(df[col], alpha=0.05, figsize=(10, 10));
-    df[col].corr()   
-    
-def df_k(df):
-    st.subheader("Метод k средних:")
-    st.write("Этот раздел содержит кластерный анализ методом k средних.")
-    col=['Review', 'Star', 'ordersCount']
-    # загружаем библиотеку препроцесинга данных
-    # эта библиотека автоматически приведен данные к нормальным значениям
-    from sklearn import preprocessing
-    dataNorm = preprocessing.MinMaxScaler().fit_transform(df[col].values)   
+# Функция для визуализации распределения выбранных категорий и выделения выбросов
+def data_visualization(data):
+    st.subheader("Визуализация:")
+    st.write("Этот раздел содержит визуализацию данных для анализа распределения и выявления выбросов.")
 
-    # Вычислим расстояния между каждым набором данных,
-    # т.е. строками массива data_for_clust
-    # Вычисляется евклидово расстояние (по умолчанию)
-    data_dist = pdist(dataNorm, 'euclidean')
-    
-    data_linkage = linkage(data_dist, method='average')  
+    # Выбор категорий для визуализации
+    format = []
+    for i in data.columns:
+        if data[i].dtypes == 'float64' or data[i].dtypes == 'int64':
+            format.append(i)
 
-    # Метод локтя. Позволячет оценить оптимальное количество сегментов.
-    # Показывает сумму внутри групповых вариаций
-    last = data_linkage[-10:, 2]
-    last_rev = last[::-1]
-    idxs = np.arange(1, len(last) + 1)
-    plt.plot(idxs, last_rev)
-    
-    acceleration = np.diff(last, 2)  
-    acceleration_rev = acceleration[::-1]
-    plt.plot(idxs[:-2] + 1, acceleration_rev)
+    corr_matrix = data.loc[:, format].corr()
+    print(corr_matrix)
+
+    plt.figure(figsize=(10, 5), dpi= 80)
+    sns.heatmap(corr_matrix, annot=True)
+
+    # Выбираем категориальные колонки
+    categorical_columns = [column for column in data.columns if data[column].dtype == 'object'] 
+    # Выбираем цифровые столбцы в список
+    numerical_columns = [column for column in data.columns if data[column].dtype != 'object']
+
+    print(numerical_columns)
+
+    for column in categorical_columns:
+        print(f'Признак: {column}, кол-во уникальных значений: {data_analysis[column].nunique()}')
+        print('Уникальные значения: ')
+        print(data[column].value_counts(), '\n')
+
+# Функция для обучения и оценки классификационной модели
+def train_and_evaluate_model(data):
+    st.subheader("Обучение и оценка результатов прогнозирования:")
+    st.write("Этот раздел содержит обучение классификационной модели и оценку ее результатов.")
+
+    print("Color Count\n-----------------------------\n", 
+      data['Цвет_звезды'].value_counts())
+    plt.figure(figsize = (13, 6)) # это пустой квадрат размером 13х6
+
+    print("\n\n")
+
+    plt.figure(figsize = (13, 6))
+    color = pd.DataFrame(data['Цвет_звезды'].value_counts().sort_values(ascending=False)) # маленький датасет для формирования цветов
+    ax = sns.barplot(x = color.index, y = 'Цвет_звезды' , data = color)
+    plt.title("Визуализация цветов звезды")
+    plt.ylabel('Цвет_звезды')
+    ax = ax.set_xticklabels(ax.get_xticklabels(), rotation = 90)    
+
+def t1(data):
+    print("Class Count\n-------------------\n", 
+        data['Спектральный_класс'].value_counts())
+
+    spectral_class = pd.DataFrame(data['Спектральный_класс'].value_counts().sort_values(ascending=False))
+
+    print("\n\n")
+
+    plt.figure(figsize=(10, 6))
+    ax = sns.barplot(x = spectral_class.index, y = 'Спектральный_класс', 
+                    data = spectral_class)
+    plt.title("Визуализация Спектральный_класс")
+    plt.ylabel('Спектральный_класс')
     plt.show()
-    k = acceleration_rev.argmax() + 2 
-    print("Рекомендованное количество кластеров:", k)
 
-    nClust=6
+    print('\n\n\n\n\n')
 
-    # строим кластеризаци методом KMeans
-    km = KMeans(n_clusters=nClust).fit(dataNorm)
-    # выведем полученное распределение по кластерам
-    # так же номер кластера, к котрому относится строка, так как нумерация начинается с нуля, выводим добавляя 1
-    km.labels_ +1
-    st.write("Отношение обзоров и количесва заказов.")
-    x=0 # Чтобы построить диаграмму в разных осях, меняйте номера столбцов
-    y=2 #
-    centroids = km.cluster_centers_
-    plt.figure(figsize=(10, 8))
-    plt.scatter(dataNorm[:,x], dataNorm[:,y], c=km.labels_, cmap='flag')
-    plt.scatter(centroids[:, x], centroids[:, y], marker='*', s=300,
-                c='r', label='centroid')
-    plt.xlabel(col[x])
-    plt.ylabel(col[y]);
+    plt.figure(figsize=(10, 6))
+    plt.pie(data['Спектральный_класс'].value_counts(), labels=data['Спектральный_класс'].value_counts())
     plt.show()
-    st.write("Таблица средних по кластерам.")
-    # к оригинальным данным добавляем номера кластеров
-    df['KMeans']=km.labels_+1
-    res=df.groupby('KMeans')[col].mean()
-    res['Количество']=df.groupby('KMeans').size().values
-    res
+
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data['Тип_звезды'])
+    plt.show()
+
+    print('\n\n\n\n\n')
+
+    plt.figure(figsize=(10, 6))
+    plt.pie(data['Тип_звезды'].value_counts(), labels=data['Тип_звезды'].value_counts())
+    plt.show()
+ 
+
+def t2(data):
+    numerical_columns = [column for column in data.columns if data[column].dtype != 'object']
+    for item in numerical_columns:
+        print(f'Визуализация ящиковой диаграммы {item}')
+        sns.boxplot(x=item, data=data)
+        plt.title(item)
+        plt.show()
+        print('\n\n\n')
+
+        plt.scatter(x=data[item], y=data.index)
+        plt.title(item)
+        plt.show()
+        print('\n\n\n')
 # Заголовок
-st.title("Кластерный анализ")
-    
+st.title("Анализ данных")
+
 # Загрузка данных
 data = load_data()
 
@@ -161,10 +179,14 @@ st.write(data)
 
 # Описание полей данных
 show_data_description(data)
-
+data_analysis(data)
 # Анализ и предобработка данных
-data_preporation(data)
+data_analysis(data)
 
 # Визуализация
-df_k(data)
+data_visualization(data)
 
+# Обучение и оценка результатов прогнозирования
+train_and_evaluate_model(data)
+t1(data)
+t2(data)
